d0ec916b81221c59bf804f6a419fccb31bf2d7d4
MaryBenitez <marisol.dominguezxp@gmail.com>
2018-05-26 21:58:10 -0600

    Clase de agregar completa.

--- app/src/main/java/com/example/maris/tarealabo/Fragmentos/AgregarActivity.java
+++ app/src/main/java/com/example/maris/tarealabo/Fragmentos/AgregarActivity.java
@@ -7,7 +7,11 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
+import com.example.maris.tarealabo.Clases.Alumno;
+import com.example.maris.tarealabo.DB.DBHelper;
 import com.example.maris.tarealabo.R;
 
 
@@ -20,6 +24,11 @@ import com.example.maris.tarealabo.R;
  * create an instance of this fragment.
  */
 public class AgregarActivity extends Fragment {
+
+    EditText nombre;
+    EditText carnet;
+    Button agregar;
+
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -65,8 +74,24 @@ public class AgregarActivity extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        DBHelper.getInstance(getContext());
+        View v = inflater.inflate(R.layout.fragment_agregar,container,false);
+
+        nombre=v.findViewById(R.id.txtnombre);
+        carnet=v.findViewById(R.id.txtid);
+        agregar=v.findViewById(R.id.btnregistrar);
+
+        agregar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DBHelper.mydb.AgregarAlumno(new Alumno(nombre.getText().toString(),carnet.getText().toString(),""));
+                Vaciar();
+            }
+        });
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_agregar, container, false);
+        return v;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
@@ -106,5 +131,10 @@ public class AgregarActivity extends Fragment {
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
+    }
+
+    private void Vaciar() {
+        carnet.setText("");
+        nombre.setText("");
     }
 }
