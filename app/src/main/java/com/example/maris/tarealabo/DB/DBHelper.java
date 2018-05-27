f2616e0ba7bb9dbc9149dd139d0505cf4b5bc601
MaryBenitez <marisol.dominguezxp@gmail.com>
2018-05-26 21:43:49 -0600

    Se crearon las clases para agregar, modificar, borrar y buscar alumnos.

--- app/src/main/java/com/example/maris/tarealabo/DB/DBHelper.java
+++ app/src/main/java/com/example/maris/tarealabo/DB/DBHelper.java
@@ -2,11 +2,14 @@ package com.example.maris.tarealabo.DB;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.widget.Toast;
 
 import com.example.maris.tarealabo.Clases.Alumno;
+
+import java.util.ArrayList;
 
 public class DBHelper extends SQLiteOpenHelper {
 
@@ -52,6 +55,57 @@ public class DBHelper extends SQLiteOpenHelper {
         values.put(CAMPO_NOTA,alumno.getNota());
         db.insert(TABLA_ALUMNO,null,values);
         Toast.makeText(context,"Alumno agregado con exito",Toast.LENGTH_SHORT).show();
+        return true;
+    }
+
+    public ArrayList<Alumno> ObtenerAlumno(){
+
+        Cursor cursor = db.rawQuery("SELECT * FROM "+TABLA_ALUMNO,null);
+        ArrayList<Alumno> alumnos = new ArrayList<Alumno>();
+
+        while (cursor.moveToNext()){
+            alumnos.add(new Alumno(cursor.getString(0),cursor.getString(1),
+                    cursor.getString(2)));
+        }
+
+        return alumnos;
+    }
+
+    public Alumno BuscarAlumno(String carnet){
+
+        Alumno alumno;
+        String [] parametros = {carnet};
+        String [] campos = {CAMPO_NOMBRE,CAMPO_NOTA};
+
+        try {
+            Cursor cursor = db.query(TABLA_ALUMNO,campos,CAMPO_ID+"=?",parametros,null,null,null);
+            cursor.moveToFirst();
+            alumno=new Alumno(cursor.getString(0),carnet,cursor.getString(1));
+        }
+        catch (Exception e){
+            Toast.makeText(context,"Alumno no encontrado", Toast.LENGTH_SHORT).show();
+            alumno=null;
+        }
+        return alumno;
+    }
+
+    public boolean ModificarAlumno(Alumno alumno){
+
+        String [] parametros = {alumno.getCarnet()};
+        String[] campos = {CAMPO_NOMBRE,CAMPO_NOTA};
+
+        ContentValues values = new ContentValues();
+        values.put(CAMPO_NOMBRE,alumno.getNombre());
+        values.put(CAMPO_NOTA,alumno.getNota());
+        db.update(TABLA_ALUMNO,values,CAMPO_ID+"=?",parametros);
+        Toast.makeText(context,"Alumno Modificado",Toast.LENGTH_SHORT).show();
+        return true;
+    }
+
+    public boolean BorrarAlumno(String carne){
+        String[] parametros = {carne};
+        db.delete(TABLA_ALUMNO,CAMPO_ID+"=?",parametros);
+        Toast.makeText(context,"Alumno eliminado",Toast.LENGTH_SHORT).show();
         return true;
     }
 
