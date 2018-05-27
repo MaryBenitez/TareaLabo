9fecd99f125065a5e62648dbf4d482ff1c899f70
MaryBenitez <marisol.dominguezxp@gmail.com>
2018-05-26 23:00:19 -0600

    Clase de mostrar completa.

--- app/src/main/java/com/example/maris/tarealabo/Fragmentos/MostrarActivity.java
+++ app/src/main/java/com/example/maris/tarealabo/Fragmentos/MostrarActivity.java
@@ -4,11 +4,18 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.maris.tarealabo.Adapter.AlumnoAdapter;
+import com.example.maris.tarealabo.Clases.Alumno;
+import com.example.maris.tarealabo.DB.DBHelper;
 import com.example.maris.tarealabo.R;
+
+import java.util.ArrayList;
 
 
 /**
@@ -20,6 +27,12 @@ import com.example.maris.tarealabo.R;
  * create an instance of this fragment.
  */
 public class MostrarActivity extends Fragment {
+
+    RecyclerView rv;
+    LinearLayoutManager layoutManager;
+    ArrayList<Alumno> alumno;
+    AlumnoAdapter adapter;
+
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -65,8 +78,17 @@ public class MostrarActivity extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        View v = inflater.inflate(R.layout.fragment_modificar,container,false);
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_mostrar, container, false);
+
+        rv=v.findViewById(R.id.rv);
+        alumno= DBHelper.mydb.ObtenerAlumno();
+        adapter=new AlumnoAdapter(alumno,getContext());
+        rv.setLayoutManager(new LinearLayoutManager(getContext()));
+        rv.setAdapter(adapter);
+
+        return v;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
