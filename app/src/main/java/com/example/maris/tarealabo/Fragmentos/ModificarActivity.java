a84a947b1156bbaa966706e33aa111e99b84c11a
MaryBenitez <marisol.dominguezxp@gmail.com>
2018-05-26 22:48:13 -0600

    Clase de modificar completa.

--- app/src/main/java/com/example/maris/tarealabo/Fragmentos/ModificarActivity.java
+++ app/src/main/java/com/example/maris/tarealabo/Fragmentos/ModificarActivity.java
@@ -3,11 +3,18 @@ package com.example.maris.tarealabo.Fragmentos;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.example.maris.tarealabo.Clases.Alumno;
+import com.example.maris.tarealabo.DB.DBHelper;
 import com.example.maris.tarealabo.R;
 
 
@@ -20,6 +27,16 @@ import com.example.maris.tarealabo.R;
  * create an instance of this fragment.
  */
 public class ModificarActivity extends Fragment {
+
+    Button buscar;
+    Button modificar;
+    Button eliminar;
+    Button limpiar;
+
+    EditText carnet;
+    EditText nombre;
+    EditText nota;
+
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -65,8 +82,11 @@ public class ModificarActivity extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        View v = inflater.inflate(R.layout.fragment_modificar,container,false);
+        DBHelper.getInstance(getContext());
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_modificar, container, false);
+        return v;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
@@ -106,5 +126,64 @@ public class ModificarActivity extends Fragment {
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        carnet = view.findViewById(R.id.txtidM);
+        nombre = view.findViewById(R.id.txtnombreM);
+        nota = view.findViewById(R.id.txtnotaM);
+        buscar = view.findViewById(R.id.btnmodificar);
+        modificar= view.findViewById(R.id.btnmodificar);
+        eliminar = view.findViewById(R.id.btneliminar);
+        limpiar = view.findViewById(R.id.btnlimpiar);
+
+        buscar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Alumno alumno = DBHelper.mydb.BuscarAlumno(carnet.getText().toString());
+
+                if (alumno == null){
+                    Toast.makeText(getContext(), "Alumno no encontrado", Toast.LENGTH_SHORT).show();
+
+                }
+                else{
+                    nota.setText(alumno.getNota());
+                    nombre.setText(alumno.getNombre());
+                }
+            }
+        });
+
+        modificar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Alumno alumno = DBHelper.mydb.BuscarAlumno(carnet.getText().toString());
+                alumno.setNota(nota.getText().toString());
+                DBHelper.mydb.ModificarAlumno(alumno);
+            }
+        });
+
+        eliminar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DBHelper.mydb.BorrarAlumno(carnet.getText().toString());
+                Vaciar();
+            }
+        });
+
+        limpiar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Vaciar();
+            }
+        });
+    }
+
+    private void Vaciar() {
+        carnet.setText("");
+        nombre.setText("");
+        nota.setText("");
     }
 }
